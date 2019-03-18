a0943bba001ec88fa1dc489b79bc3baedad61355
smk87 <30999133+smk87@users.noreply.github.com>
2019-03-18 17:31:57 +0600

    Update keys_prod.js

--- key/keys_prod.js
+++ key/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI:
+    "mongodb://admin:admin@easybill-shard-00-00-zrnt8.mongodb.net:27017,easybill-shard-00-01-zrnt8.mongodb.net:27017,easybill-shard-00-02-zrnt8.mongodb.net:27017/easybill?ssl=true&replicaSet=easyBill-shard-0&authSource=admin&retryWrites=true",
   secret: process.env.SECRET
 };
